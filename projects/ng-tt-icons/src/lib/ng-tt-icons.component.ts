a6483b1760d29c4cfb260c55f97742779a6727e3
Saranya G <saranya.govindaraj@trustrace.com>
2023-04-23 20:22:05 +0530

    service and component done

--- projects/ng-tt-icons/src/lib/ng-tt-icons.component.ts
+++ projects/ng-tt-icons/src/lib/ng-tt-icons.component.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  Optional
+} from '@angular/core';
+import { TTIconRegistry } from './ng-tt-icons.service';
 
 @Component({
-  selector: 'lib-ng-tt-icons',
+  selector: 'tt-icons',
   template: `
-    <p>
-      ng-tt-icons works!
-    </p>
+    <ng-content></ng-content>
   `,
-  styles: [
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NgTtIconsComponent implements OnInit {
+  private svgIcon?: SVGElement;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input()
+  set name(iconName: string) {
+    if (this.svgIcon) {
+      this.element.nativeElement.removeChild(this.svgIcon);
+    }
+    const svgData = this.ttIconRegistry.getIcon(iconName);
+    this.svgIcon = this.svgElementFromString(<string>svgData);
+    this.element.nativeElement.appendChild(this.svgIcon);
   }
 
+  constructor(
+    private element: ElementRef,
+    private ttIconRegistry: TTIconRegistry,
+    @Optional() @Inject(DOCUMENT) private document: any
+  ) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  private svgElementFromString(svgContent: string): SVGElement {
+    const div = this.document.createElement('DIV');
+    div.innerHTML = svgContent;
+    return (
+      div.querySelector('svg') ||
+      this.document.createElementNS('http://www.w3.org/2000/svg', 'path')
+    );
+  }
 }
--- projects/ng-tt-icons/src/lib/ng-tt-icons.service.ts
+++ projects/ng-tt-icons/src/lib/ng-tt-icons.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { TTIcon } from './tt-icon.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NgTtIconsService {
+export class TTIconRegistry {
+  private registry = new Map<string, string>();
 
-  constructor() { }
+  public registerIcons(icons: TTIcon[]): void {
+    icons.forEach((icon: TTIcon) => this.registry.set(icon.name, icon.data));
+  }
+
+  public getIcon(iconName: string): string | undefined {
+    if (!this.registry.has(iconName)) {
+      console.warn(
+        `We could not find the dinosaur Icon with the name ${iconName}, did you add it to the Icon registry?`
+      );
+    }
+    return this.registry.get(iconName);
+  }
 }
