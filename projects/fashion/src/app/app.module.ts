4474aa7a1bfd9c19b750642903f7c5c5649b50c2
Saranya G <saranya.govindaraj@trustrace.com>
2023-04-24 09:53:45 +0530

    tticom module imported

--- projects/fashion/src/app/app.module.ts
+++ projects/fashion/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  NgTtIconsModule,
+  TTIconRegistry
+} from 'projects/ng-tt-icons/src/public-api';
+import {
+  ttIconIconTakeBackFacility,
+  ttIconIconUploadImage
+} from 'projects/ng-tt-icons/src/lib/tt-icon.model';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgTtIconsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private ttIconRegistry: TTIconRegistry) {
+    this.ttIconRegistry.registerIcons([
+      ttIconIconTakeBackFacility,
+      ttIconIconUploadImage
+    ]);
+  }
+}
